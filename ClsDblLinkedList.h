14549deb1957565a94655b8af5978e697263c789
unknown <bn866544@gmail.com>
2026-01-27 02:41:48 +0200

    Implement new features and improvements in data structures

--- ClsDblLinkedList.h
+++ ClsDblLinkedList.h
@@ -4,6 +4,10 @@ using namespace std;
 template <class T>
 class ClsDblLinkedList
 {
+
+protected:
+   int _size;
+
 public:
    class Node
    {
@@ -12,19 +16,23 @@ public:
       Node *Next;
       Node *prev;
    };
-   Node *head = NULL;
 
+private:
+   Node *_head = NULL;
+
+public:
    void InsertAtBeginning(T data)
    {
       Node *NewNode = new Node();
       NewNode->data = data;
       NewNode->prev = NULL;
-      NewNode->Next = head;
-      if (head != NULL)
+      NewNode->Next = _head;
+      if (_head != NULL)
       {
-         head->prev = NewNode;
+         _head->prev = NewNode;
       }
-      head = NewNode;
+      _head = NewNode;
+      _size++;
    };
    void InsertAfter(Node *find, T data)
    {
@@ -39,29 +47,32 @@ public:
          find->Next->prev = NewNode;
       }
       find->Next = NewNode;
+      _size++;
    };
    void InsertEnd(T data)
    {
       Node *NewNode = new Node();
       NewNode->data = data;
       NewNode->Next = NULL;
-      if (head == NULL)
+      if (_head == NULL)
       {
          NewNode->prev = NULL;
-         head = NewNode;
+         _head = NewNode;
+         _size++;
          return;
       }
-      Node *Last = head;
+      Node *Last = _head;
       while (Last->Next != NULL)
       {
          Last = Last->Next;
       }
       NewNode->prev = Last;
       Last->Next = NewNode;
+      _size++;
    };
    Node *find(T data) const
    {
-      Node *Current = head;
+      Node *Current = _head;
       while (Current != NULL)
       {
          if (Current->data == data)
@@ -74,10 +85,10 @@ public:
    };
    void DeleteNode(T data)
    {
-      if (head == NULL)
+      if (_head == NULL)
          return;
 
-      Node *Current = head;
+      Node *Current = _head;
 
       while (Current != NULL)
       {
@@ -86,10 +97,10 @@ public:
          {
             if (Current->prev == NULL)
             {
-               head = Current->Next;
-               if (head != NULL)
+               _head = Current->Next;
+               if (_head != NULL)
                {
-                  head->prev = NULL;
+                  _head->prev = NULL;
                }
             }
             else if (Current->Next == NULL)
@@ -105,41 +116,104 @@ public:
          }
          Current = NextNode;
       }
+
+      _size--;
    };
    void DeleteFrist()
    {
-      if (head == NULL)
+      if (_head == NULL)
          return;
-      Node *temp = head;
-      head = head->Next;
-      if (head != NULL)
+      Node *temp = _head;
+      _head = _head->Next;
+      if (_head != NULL)
       {
-         head->prev = NULL;
+         _head->prev = NULL;
       }
       delete temp;
+      _size--;
    };
    void DeleteLast()
    {
-      if (head == NULL)
+      if (_head == NULL)
          return;
-      Node *temp = head;
-      if (head->Next == NULL)
+      Node *temp = _head;
+      if (_head->Next == NULL)
       {
-         delete head;
-         head = NULL;
+         delete _head;
+         _head = NULL;
+         _size--;
          return;
       }
-
       while (temp->Next != NULL)
       {
          temp = temp->Next;
       }
       temp->prev->Next = NULL;
       delete temp;
+      _size--;
    };
+   int size()
+   {
+
+      return _size;
+   }
+   bool IsEmpty()
+   {
+
+      // return (_head==NULL);
+      return _size == 0 ? true : false;
+   }
+   void Clear()
+   {
+      while (_size > 0)
+      {
+         DeleteFrist();
+      }
+   }
+   Node *GetNode(int index)
+   {
+      if (_head == NULL || _size < index)
+         return NULL;
+      int counter = 0;
+      Node *Current = _head;
+      while (Current != NULL)
+      {
+         counter++;
+         if (counter == index)
+            return Current;
+         Current = Current->Next;
+      }
+      return NULL;
+   }
+   T GetItem(int index)
+   {
+      Node *item = GetNode(index);
+      if (item == NULL)
+      {
+         return T();
+      }
+      else
+      {
+         return item->data;
+      }
+   }
+   void Reverse()
+   {
+      Node *Current = _head;
+      Node *temp = NULL;
+      while (Current != NULL)
+      {
+         temp = Current->prev;
+         Current->prev = Current->Next;
+         Current->Next = temp;
+         Current = Current->prev;
+      }
+      if (temp != NULL)
+         _head = temp->prev;
+   }
    void printList()
    {
-      Node *temp = head;
+      Node *temp = _head;
       while (temp != NULL)
       {
          cout << "(" << temp->data << ")";
@@ -147,4 +221,24 @@ public:
       }
       cout << endl;
    };
+
+   void PrintReverse()
+   {
+      if (_head == NULL)
+      {
+         return;
+      }
+      Node *last = _head;
+      while (last->Next != NULL)
+      {
+         last = last->Next;
+      }
+
+      while (last != NULL)
+      {
+         cout << "(" << last->data << ")";
+         last = last->prev;
+      }
+      cout << endl;
+   }
 };
--- Main.c++
+++ Main.c++
@@ -4,12 +4,30 @@ using namespace std;
 int main()
 {
     ClsDblLinkedList<int> MyDblLinkedList;
+    if (MyDblLinkedList.IsEmpty())
+    {
+        cout << "\n yes,this linkedlist is empty\n";
+    }
+    else
+    {
+        cout << "\n No, this list is'n empty\n ";
+    }
+
     MyDblLinkedList.InsertAtBeginning(5);
     MyDblLinkedList.InsertAtBeginning(4);
     MyDblLinkedList.InsertAtBeginning(3);
     MyDblLinkedList.InsertAtBeginning(2);
     cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
+    if (MyDblLinkedList.IsEmpty())
+    {
+        cout << "\n yes,this linkedlist is empty\n";
+    }
+    else
+    {
+        cout << "\n No, this list is'n empty\n ";
+    }
+
     MyDblLinkedList.DeleteFrist();
 
     cout << "the elements in linkedlist are :-\n";
@@ -22,21 +40,54 @@ int main()
 
     cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
-     ClsDblLinkedList<int>::Node *FindAfter = MyDblLinkedList.find(3);
-    MyDblLinkedList.InsertAfter(FindAfter,1);
+    ClsDblLinkedList<int>::Node *FindAfter = MyDblLinkedList.find(3);
+    MyDblLinkedList.InsertAfter(FindAfter, 1);
     cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
     ClsDblLinkedList<int>::Node *Find = MyDblLinkedList.find(1);
     ClsDblLinkedList<int>::Node *Find1 = MyDblLinkedList.find(2);
-     cout << "the elements in linkedlist are :-\n";
+    cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
     MyDblLinkedList.DeleteNode(4);
-     cout << "the elements in linkedlist are :-\n";
+    cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
     MyDblLinkedList.InsertEnd(7);
-     cout << "the elements in linkedlist are :-\n";
+    cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
+    cout << "The Count of Element is " << MyDblLinkedList.size() << endl;
     MyDblLinkedList.DeleteNode(1);
+    cout << "the elements in linkedlist are :-\n";
+    MyDblLinkedList.printList();
+    cout << "The Count of Element is " << MyDblLinkedList.size() << endl;
+    if (MyDblLinkedList.IsEmpty())
+    {
+        cout << "\n yes,this linkedlist is empty\n";
+    }
+    else
+    {
+        cout << "\n No, this list is'n empty\n ";
+    }
+  
+    MyDblLinkedList.InsertAtBeginning(5);
+    cout << "the elements in linkedlist are :-\n";
+    MyDblLinkedList.printList();
+   
+    if (MyDblLinkedList.IsEmpty())
+
+    {
+        cout << "\n yes,this linkedlist is empty\n";
+    }
+    else
+    {
+        cout << "\n No, this list is'n empty\n ";
+    }
+    
+   
+     cout << "the elements in linkedlist are :-\n";
+    MyDblLinkedList.Reverse();
+    
      cout << "the elements in linkedlist are :-\n";
     MyDblLinkedList.printList();
+
+cout << "the value of item 2 is "<<MyDblLinkedList.GetItem(8);
 };
